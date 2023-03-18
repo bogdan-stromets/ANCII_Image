bf2a86f016ce7916a443d53966fe97eb8c4d5cc1
Bogdan Stromec <grom005521@gmail.com>
2023-03-19 01:04:26 +0200

    Converter

---
+++ ANCII_Image/Converter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ANCII_Image
+{
+    public class Converter
+    {
+        private readonly Bitmap b;
+        private readonly char[] ancii_arr = { '.', ',', ':', '+', '*', '?','%','$','@'};
+
+        public Converter(Bitmap b)
+        {
+            this.b = b;
+        }
+
+        public char[][] Convert()
+        {
+            char[][] res = new char[b.Height][];
+            for (int y = 0; y < b.Height; y++)
+            {
+                res[y] = new char[b.Width];
+                for (int x = 0; x < b.Width; x++)
+                {
+                    int ind = (int)Map(b.GetPixel(x,y).R,0,255,0,ancii_arr.Length - 1);
+                    res[y][x] = ancii_arr[ind];
+                }
+            }
+            
+            return res;
+        }
+
+        float Map(float valueToMap, float start_1, float stop_1, float start_2, float stop_2)
+        {
+            return ((valueToMap - start_1) / (stop_1 - start_1) * (stop_2 - start_2) + start_2);
+        }
+        
+    }
+}
---
+++ ANCII_Image/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace ANCII_Image
+{
+    internal class Program
+    {
+        private const double WIDTH_OFFSET = 1.5;
+        private const int MAX_WIDTH = 200;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var openFile = new OpenFileDialog()
+            {
+                Filter = "Images | *.bmp; *.jpg; *.png; *.JPEG"
+            };
+
+            while (true)
+            {
+                Console.ReadLine();
+                if (openFile.ShowDialog() != DialogResult.OK)
+                    continue;
+                Console.Clear();
+                
+                Bitmap bitmap = new Bitmap(openFile.FileName);
+                bitmap = ResizeBitmap(bitmap);
+                bitmap.ToGrayScale();
+                Converter converter = new Converter(bitmap);
+                var rows = converter.Convert();
+
+                foreach (var item in rows)
+                    Console.WriteLine(item);
+
+                Console.SetCursorPosition(0, 0);
+            }
+        }
+
+        static Bitmap ResizeBitmap(Bitmap b)
+        { 
+            var height = b.Height / WIDTH_OFFSET * MAX_WIDTH  / b.Width;
+            if (b.Height > height || b.Width > MAX_WIDTH)
+                b = new Bitmap(b, new Size(MAX_WIDTH,(int)height));    
+            return b;
+        }
+    }
+}
